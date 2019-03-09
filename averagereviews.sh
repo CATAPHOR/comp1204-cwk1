41d4abd95b84497adeb5cfb098d02ed910c85913
Adam Gafar <aeg3g18@soton.ac.uk>
2019-03-09 00:40:25 +0000

    Improved efficiency of averagereviews.sh; grep only invoked once.

--- averagereviews.sh
+++ averagereviews.sh
@@ -2,10 +2,9 @@
 
 for file in $1/*
 do
-	num=$(grep '<Overall>' $file | wc -l)
-	avg=$(grep '<Overall>' $file | sed 's|<Overall>||' | awk -v num="$num" '{t+=$1}END{printf "%0.2f", t/num;}')
+	lines=$(grep '<Overall>' $file)
+	num=$(echo "$lines" | wc -l)
+	avg=$(echo "$lines" | sed 's|<Overall>||' | awk -v num="$num" '{t+=$1}END{printf "%0.2f", t/num;}')
 	name=$(echo $file | sed 's|^.*/||; s|.dat||')
-	echo "$name $avg" 
+	echo "$name $avg"
 done | sort -nrk2
-
-
