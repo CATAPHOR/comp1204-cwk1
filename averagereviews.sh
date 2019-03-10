87b3918790c14cf4f4f8d93b8030da8cf3477e36
Adam Gafar <aeg3g18@soton.ac.uk>
2019-03-10 04:20:41 +0000

    averagereviews.sh: Code formatting (only one empty line deemed beneficial to readability); simplified grep search string.

--- averagereviews.sh
+++ averagereviews.sh
@@ -2,11 +2,10 @@
 
 for file in $1/*
 do
-	name=$(echo $file | sed 's|^.*/||; s|.dat||')
-
-	scores=$(grep '^<Overall>[0-5]' $file | sed 's|<Overall>||')
+	scores=$(grep '^<Overall>' $file | sed 's|<Overall>||')
 	num=$(echo "$scores" | wc -l)
 	avg=$(echo "$scores" | awk -v num="$num" '{t+=$1}END{printf "%0.2f", t/num;}')
 
+	name=$(echo $file | sed 's|^.*/||; s|.dat||')
 	echo "$name $avg"
 done | sort -nrk2
